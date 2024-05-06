28c13f731221f4ab89b4fb45acc02e8615394dc1
badr <badr@test.com>
2024-05-06 02:55:17 +0100

    copy

--- src/pages/Home/components/firstSection.jsx
+++ src/pages/Home/components/firstSection.jsx
@@ -1,5 +1,7 @@
 
 import { useState } from "react";
+import { CopyToClipboard } from 'react-copy-to-clipboard';
+
 import './firstSection.scss'
 export const FirstSection = () => {
     const [text, setText] = useState("");
@@ -7,6 +9,8 @@ export const FirstSection = () => {
 
     const [text2, setText2] = useState("");
     const [result2, setResult2] = useState("");
+
+    const [copyStatus, setCopyStatus] = useState(false);
 
     const [replacements, setReplacements] = useState({
         "q": "0",
@@ -90,22 +94,39 @@ export const FirstSection = () => {
         setText2(event.target.value); // Update state with the new value from the input
     };
 
+    const onCopyText = () => {
+        setCopyStatus(true);
+        setTimeout(() => setCopyStatus(false), 2000); // Reset status after 2 seconds
+      };
+
     return (
         <>
             <nav className='p-5 border-b-[1px] border-[#ffffff41]'>
                 <h1 className='text-center text-4xl font-bold text-white jaini-purva-regular'>Propreypter</h1>
             </nav>
-            <div className="flex flex-row justify-evenly items-center gap-5 h-full p-10">
+            <div className="flex md:flex-row flex-col justify-evenly items-center gap-5 h-full p-10">
                 <div className='flex flex-col gap-5 w-full'>
                     <input className='bg-[#192646] rounded-lg p-3 h-48 w-full border-t-2 border-[#293445] opacity-90 text-white' type="text" onChange={handleChange} value={text} />
                     <button onClick={handleClick1} className='bg-[#293655] hover:bg-[#38445f] p-5 rounded-lg font-bold text-white jaini-purva-regular text-2xl'>Encypt</button>
-                    <input className='bg-[#192646] rounded-lg p-3 h-48 w-full border-t-2 border-[#293445] opacity-90 text-white' type="text" value={result} disabled />
+
+                    <div className="relative">
+                        <input className='bg-[#192646] rounded-lg p-3 h-48 w-full border-t-2 border-[#293445] opacity-90 text-white cursor-text' type="text" value={result} disabled />
+                        <CopyToClipboard text={result} onCopy={onCopyText}>
+                            <button className="bg-[#293655] hover:bg-[#38445f] p-2 rounded-lg font-bold text-white jaini-purva-regular text-xl w-fit absolute right-0 top-0 mt-2 mr-2">Copy</button>
+                        </CopyToClipboard>
+                    </div>
                 </div>
 
                 <div className='flex flex-col gap-5 w-full'>
                     <input className='bg-[#211e3b] rounded-lg p-3 h-48 w-full border-t-2 border-[#293445] opacity-90 text-white' type="text" onChange={handleChange2} value={text2} />
                     <button onClick={handleClick2} className='bg-[#382955] hover:bg-[#46385f] p-5 rounded-lg font-bold text-white jaini-purva-regular text-2xl'>Decrypt</button>
-                    <input className='bg-[#211e3b] rounded-lg p-3 h-48 w-full border-t-2 border-[#293445] opacity-90 text-white' type="text" value={result2} disabled />
+
+                    <div className="relative">
+                        <input className='bg-[#211e3b] rounded-lg p-3 h-48 w-full border-t-2 border-[#293445] opacity-90 text-white cursor-text' type="text" value={result2} disabled />
+                        <CopyToClipboard text={result2} onCopy={onCopyText}>
+                            <button className="bg-[#382955] hover:bg-[#46385f] p-2 rounded-lg font-bold text-white jaini-purva-regular text-xl w-fit absolute right-0 top-0 mt-2 mr-2">Copy</button>
+                        </CopyToClipboard>
+                    </div>
                 </div>
             </div>
         </>
